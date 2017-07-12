371423c4467173f4e3024967f61d087981ab7712
someone <someone@example.com>
2017-07-12 23:23:14 +0900

    Add fragment dynamically

--- app/src/main/java/com/example/n/simpleaccountbook/MainActivity.java
+++ app/src/main/java/com/example/n/simpleaccountbook/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.n.simpleaccountbook;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Fragmentを作成します
+        BlankFragment fragment = new BlankFragment();
+        // Fragmentの追加や削除といった変更を行う際は、Transactionを利用します
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        // 新しく追加を行うのでaddを使用します
+        // 他にも、メソッドにはreplace removeがあります
+        // メソッドの1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
+        transaction.add(R.id.container, fragment);
+        // 最後にcommitを使用することで変更を反映します
+        transaction.commit();
     }
 }
